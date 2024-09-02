ad80e5837f6172da2c5bf7321e925d0cf84b3025
rabaou-ibrahim <rabaou.ibrahim@laplateforme.io>
2024-09-02 15:37:21 +0200

    Changement d'architecture

--- tests/integrate/SignupIntegrateTest.php
+++ tests/integrate/SignupIntegrateTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 require './controllers/UsersController.php';
 
 use PHPUnit\Framework\TestCase;
-use function PHPUnit\Framework\assertEquals;
 use function PHPUnit\Framework\assertSame;
 
 class SignupIntegrateTest extends TestCase
